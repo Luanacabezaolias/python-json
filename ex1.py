5fcb1cab0c3fffbbf9463b5192464c908c825c54
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-24 09:45:39 -0300

    exercicios

--- ex1.py
+++ ex1.py
@@ -3,5 +3,24 @@ Cada linha do arquivo apresenta os dados de um aluno, no formato:
 RM,NOME,NOTA1,NOTA2,NOTA3,NOTA4
 Implemente um programa que leia este arquivo e gere um novo arquivo JSON no formato:'''
 
-with open('notas.csv', 'r') as arquivo:
-    for linha in arquivo
+import json
+with open('notas.csv', 'r', encoding='utf-8') as arquivo:
+
+    alunos = {}
+    conta_linha = 1
+    for linha in arquivo:
+        if conta_linha > 1:     # ignora a primeira linha do arquivo csv
+            lista = linha.split(';')
+            print(lista)
+
+            rm = lista[0]
+            nome = lista[1]
+            notas = [float(lista[2]),float(lista[3]),float(lista[4]),float(lista[5])]
+            alunos[rm] = { 'nomes': nome, 'notas': notas}   #insere no dicionario
+        conta_linha += 1
+
+print(alunos)
+
+with open('notas.json', 'w', encoding='utf-8') as arquivo:
+    json.dump(alunos, arquivo, indent=4)
+
--- ex2.py
+++ ex2.py
@@ -2,18 +2,26 @@
 indivíduo com suas respectivas informações. 
 NAME,ID,FAVORITE FOOD
 Implemente um programa que a partir do arquivo indicado gere um arquivo JSON no formato:
-{
- "1": {
- "nome": "John Doe",
- "food": "pizza"
- },
- "2": {
- "nome": "Jane Smith",
- "food": "sushi"
- },
- "3": {
- "nome": "Michael Johnson",
- "food": "steak"
- }
-}
+
 '''
+import json
+
+with open('foods.csv', 'r', encoding='utf-8') as arquivo:
+    foods = {}
+    conta_linha = 1
+    for linha in arquivo:
+        if conta_linha > 1:
+            lista = linha.split(';')
+            print(lista)
+
+            id = lista[1]
+            nome = lista[0]
+            comida_favorita = lista[2]
+            foods[id] = {'nome': nome, 'food': comida_favorita}
+        conta_linha += 1
+print(foods)
+
+with open('foods.json', 'w', encoding='utf-8') as arquivo:
+    json.dump(foods,arquivo, indent=4)
+           
+
